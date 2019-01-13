70e4eea8f68ac8eb42174c29f6f6bbf67b3e09de
Quan Wang <quanw@google.com>
2019-01-12 19:37:29 -0500

    Initialize the repo with the script to compute probability of k cards containing a SET

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# SetGameSimulation
+# Simulations for the SET board game
+
+## Introduction
+
+SET is a board game where there are 81 different cards.
+
+The rules can be found here:
+
+* [English](https://puzzles.setgame.com/set/rules_set.htm)
+* [中文](https://www.setgame.com/sites/default/files/instructions/SET%20INSTRUCTIONS%20-%20CHINESE.pdf)
+
+## Least number of cards to guarantee a SET
+
+Question: If *k* cards are dealt, is it possible that there's no SETs from these cards?
+
+What's the smallest number of *k* to guarantee the existence of a SET?
+
+`set_prob.py` is a simulation program to compute this. Run it like this:
+
+```bash
+python set_prob.py --start=12 --end=18 --num_trials=100000
+```
+
+This will compute the probabilities of *k* cards containing a SET, where
+*k=12, 13, ..., 18*, and for each *k* a try 100,000 times.
+
+Use a larger value for `--num_trials` for more accurate estimations, or use
+`--num_trials=0` for an exact result (which can be very slow).
+
+### Results
+
+#### 10000 trials
+
+| k     | probability        |
+|-------|--------------------|
+| 12    | 0.968              |
+| 13    | 0.9903             |
+| 14    | 0.99789            |
+| 15    | 0.99959            |
+| 16    | 0.999999           |
+| 17    | 1.0                |
+| 18    | 1.0                |
+
+## References
+
+* [SET by BRIAN CONREY AND BRIANNA DONALDSON](https://www.mathteacherscircle.org/assets/session-materials/BConreyBDonaldsonSET.pdf)
+* [The Odds of Finding a SET in The Card Game SET](http://norvig.com/SET.html)
+* [SETs and Anti-SETs: The Math Behind the Game of SET](http://www-personal.umich.edu/~charchan/SET.pdf)
---
+++ set_prob.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import itertools
+import random
+import time
+
+
+def get_cards(x):
+    """From number to digits.
+
+    Args:
+        x: a number from 0 to 80.
+
+    Returns:
+        a tuple of four numbers corresponding to the four features
+    """
+    x1 = x // 27
+    x2 = (x % 27) // 9
+    x3 = (x % 9) // 3
+    x4 = x % 3
+    return (x1, x2, x3, x4)
+
+
+def is_set(a, b, c):
+    """Whether three cards form a set."""
+    a_cards = get_cards(a)
+    b_cards = get_cards(b)
+    c_cards = get_cards(c)
+    for i in range(4):
+        if a_cards[i] == b_cards[i] and b_cards[i] == c_cards[i]:
+            continue
+        elif (a_cards[i] != b_cards[i] and b_cards[i] != c_cards[i]
+              and c_cards[i] != a_cards[i]):
+            continue
+        else:
+            return False
+    return True
+
+
+def has_set(nums):
+    """Whether a list of numbers contains a set."""
+    assert len(nums) >= 3
+    for combination in itertools.combinations(nums, 3):
+        a, b, c = combination
+        if is_set(a, b, c):
+            return True
+    return False
+
+
+def probability_set_approx(k, num_trials=1000000):
+    """Probability of k cards containing a set, approximated."""
+    count = 0
+    for _ in range(num_trials):
+        nums = random.sample(range(81), k)
+        if has_set(nums):
+            count += 1
+    return count, num_trials
+
+
+def probability_set(k):
+    """Probability of k cards containing a set, exact number."""
+    count = 0
+    total = 0
+    for nums in itertools.combinations(range(81), k):
+        total += 1
+        if has_set(nums):
+            count += 1
+    return count, total
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--start",
+        default=12,
+        type=int,
+        help="Number of cards to start with the simulation.")
+    parser.add_argument(
+        "--end",
+        default=18,
+        type=int,
+        help="Number of cards to end with the simulation.")
+    parser.add_argument(
+        "--num_trials",
+        default=100000,
+        type=int,
+        help="Number of trials. The larger the number, "
+             "the more accurate the result is. "
+             "If this number is zero or negative, we will use accurate "
+             "simulation instead of approximated simulation.")
+    args = parser.parse_args()
+
+    for k in range(args.start, args.end + 1):
+        start_time = time.time()
+        if args.num_trials <= 0:
+            count, total = probability_set(k)
+        else:
+            count, total = probability_set_approx(k, args.num_trials)
+        end_time = time.time()
+        print("Probability of {} cards containing a set: {} / {} = {}, "
+              "time spent: {}s".format(
+                  k, count, total, count / total, end_time - start_time))
+
+
+if __name__ == "__main__":
+    main()
